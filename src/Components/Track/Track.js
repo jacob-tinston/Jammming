8f2de882dc1d721f700c93a2b560daebbeee0b95
jacob-tinston <jtinston678@gmail.com>
2021-04-01 16:04:30 +0100

    Added Track component

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -3,11 +3,10 @@ import './Track.css';
 
 class Track extends React.Component {
     renderAction() {
-        const isRemoval = false;
-        if(isRemoval) {
-            return '-';
+        if(this.props.isRemoval) {
+            return <button className="Track-action">-</button>;
         } else {
-            return '+';
+            return <button className="Track-action">+</button>;
         };
     }
 
@@ -18,7 +17,7 @@ class Track extends React.Component {
                   <h3> {/* track name will go here */}</h3>
                   <p> {/* track artist will go here */} | {/* track album will go here */}</p>
                 </div>
-                <button className="Track-action">{this.renderAction()}</button>
+                {this.renderAction()}
             </div>
         );
     }
