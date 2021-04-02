eef92389608412952cb96da3da4b90c5a915f5a4
jacob-tinston <jtinston678@gmail.com>
2021-04-02 17:56:22 +0100

    Passed and rendered results

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './SearchResults.css';
+import Tracklist from '../Tracklist/Tracklist.js';
 
 class SearchResults extends React.Component {
     render() {
         return (
             <div className="SearchResults">
                <h2>Results</h2>
-               {/* <Tracklist /> */}
+               <Tracklist tracks={this.props.searchResults} />
             </div>
         );
     }
--- src/Components/Tracklist/Tracklist.js
+++ src/Components/Tracklist/Tracklist.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './Tracklist.css';
+import Track from '../Track/Track.js';
 
 class Tracklist extends React.Component {
     render() {
         return (
-            <div className="TrackList">               
+            <div className="TrackList">
+                {
+                    this.props.tracks.map(track => {
+                        return <Track track={track} key={track.id} />;
+                    })
+                }               
             </div>
         );
     }
