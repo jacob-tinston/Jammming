d8cf8133f53d4925d4f664d57522972584cdc53d
jacob-tinston <jtinston678@gmail.com>
2021-04-03 22:47:32 +0100

    Remove tracks from playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -21,15 +21,21 @@ class App extends React.Component {
       ]
     };
     this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
   }
 
   addTrack(track) {
     let tracks = this.state.playlistTracks;
     if(tracks.find(savedTrack => savedTrack.id === track.id)) {
       return;
-    }
-    
+    }   
     tracks.push(track);   
+    this.setState({ playlistTracks: tracks });
+  }
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter(savedTrack => savedTrack.id !== track.id);
     this.setState({ playlistTracks: tracks });
   }
 
@@ -43,6 +49,7 @@ class App extends React.Component {
             <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
             <Playlist playlistName={this.state.playlistName} 
                       playlistTracks={this.state.playlistTracks} 
+                      onRemove={this.removeTrack}
             />
           </div>
         </div>
