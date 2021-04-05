a4d17b3ee4bc2af3e26c9a9422bda88b69280ca2
jacob-tinston <jtinston678@gmail.com>
2021-04-05 17:56:25 +0100

    Implement Spotify search request

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,8 +1,9 @@
 import './App.css';
 import React from 'react';
 import SearchBar from '../SearchBar/SearchBar.js';
-import SearchResults from '../SearchResults/SearchResults';
+import SearchResults from '../SearchResults/SearchResults.js';
 import Playlist from '../Playlist/Playlist.js';
+import Spotify from '../../util/Spotify.js';
 
 class App extends React.Component {
   constructor(props) {
@@ -52,7 +53,9 @@ class App extends React.Component {
   }
 
   search(term) {
-    console.log(term);
+    Spotify.search(term).then(searchResults => {
+      this.setState({ searchResults: searchResults });
+    });
   }
 
   render() {
